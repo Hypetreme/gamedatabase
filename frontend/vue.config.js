a7a88e8e1596a04b2e553a1c98c72030f9a596c7
Hypetreme <hypetremethewanderer@gmail.com>
2019-10-08 03:59:51 +0300

    .

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,25 +1,28 @@
-const BundleTracker = require('webpack-bundle-tracker');
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: "http://0.0.0.0:8080/",
   outputDir: './dist/',
 
   chainWebpack: config => {
-    config.optimization.splitChunks(false);
+
+    config.optimization
+      .splitChunks(false)
 
     config
       .plugin('BundleTracker')
-      .use(BundleTracker, [{ filename: '../frontend/webpack-stats.json' }]);
+      .use(BundleTracker, [{ filename: '../frontend/webpack-stats.json' }])
 
-    config.resolve.alias.set('__STATIC__', 'static');
+    config.resolve.alias
+      .set('__STATIC__', 'static')
 
     config.devServer
-      .public('http://localhost:8080/')
-      .host('localhost')
+      .public('http://0.0.0.0:8080')
+      .host('0.0.0.0')
       .port(8080)
       .hotOnly(true)
       .watchOptions({ poll: 1000 })
       .https(false)
-      .headers({ 'Access-Control-Allow-Origin': ["\*"] });
+      .headers({ "Access-Control-Allow-Origin": ["\*"] })
   }
 };
