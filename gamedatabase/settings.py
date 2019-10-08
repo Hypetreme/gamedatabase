e2c33408d9210d1fff0c17e8a2d9cdef2b9cc937
Hypetreme <hypetremethewanderer@gmail.com>
2019-10-08 04:49:31 +0300

    m

--- gamedatabase/settings.py
+++ gamedatabase/settings.py
@@ -130,13 +130,13 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_ROOT = (BASE_DIR + '/media/')
 MEDIA_URL = '/media/'
